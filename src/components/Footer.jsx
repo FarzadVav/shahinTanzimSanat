959fda22b5acb826e5ac85faf3e5c075b77ebf5e
FarzadVav <vav.progg.ir@gmail.com>
2023-05-04 10:09:11 -0700

    add email and phone number to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,15 +15,19 @@ export default function Footer() {
       </div>
       <div className="w-full flex flex-col justify-between items-start gap-10 p-5 pt-10
         md:flex-row md:gap-5 lg:gap-10 lg:p-10">
-        <div className="w-full flex flex-col-reverse justify-center items-center gap-5
-          md:w-1/4 md:flex-col">
+        <div className="w-full flex flex-col justify-center items-center gap-5
+          md:w-1/4">
           <img className='w-1/2' src="./../../public/imgs/logo-png.png" alt="logo" />
-          <a className="bg-blue-500 w-10 h-10 flex justify-center items-center rounded-full
-            hover:scale-110" href="#header">
-            <svg className="stroke-white w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" />
+          <span className="flex justify-center items-center gap-2">example@gnail.com
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+              <path className="fill-slate-700" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
             </svg>
-          </a>
+          </span>
+          <span className="flex justify-center items-center gap-2">0910450250
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+              <path className="fill-slate-700" d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+            </svg>
+          </span>
         </div>
         <div className="w-full flex flex-col justify-start items-start gap-5
           md:w-1/4">
