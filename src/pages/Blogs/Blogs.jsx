6ea8890c04d2b901095c7e87a75cff0d1f235a2a
FarzadVav <vav.progg.ir@gmail.com>
2023-05-05 07:41:33 -0700

    build Blogs page

---
+++ src/pages/Blogs/Blogs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Blogs() {
+  return (
+    <div className='container mt-3 flex flex-col justify-center items-center px-3
+    sm:p-0'>
+        blogs
+    </div>
+  )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home/Home"
 import Gallery from "./pages/Gallery/Gallery"
+import Blogs from "./pages/Blogs/Blogs"
 
 const routes = [
   { path: '/', element: <Home /> },
   { path: '/gallery', element: <Gallery /> },
+  { path: '/blogs', element: <Blogs /> },
 ]
 
 export default routes
