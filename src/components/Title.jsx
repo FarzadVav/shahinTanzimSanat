f616576a8e55553d2633dd046371299f4ead71f2
FarzadVav <vav.progg.ir@gmail.com>
2023-05-05 02:23:08 -0700

    change title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,6 @@
 export default function Title(props) {
   return (
-    <div className="w-full flex items-center gap-3">
+    <div className="w-full flex justify-center items-center gap-3 sm:justify-start">
       {props.svg}
       <h3 className='text-slate-700 text-xl irsB sm:text-3xl'>{props.title}</h3>
     </div>
