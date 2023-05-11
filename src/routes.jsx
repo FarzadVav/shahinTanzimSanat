dd136ef67ac6a473e3d1b25fadd20be0191d51de
FarzadVav <vav.progg.ir@gmail.com>
2023-05-10 23:55:33 -0700

    build honors page

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,13 +3,15 @@ import Gallery from "./pages/Gallery/Gallery"
 import Blogs from "./pages/Blogs/Blogs"
 import SingleBlog from "./pages/SingleBlog/SingleBlog"
 import Contact from "./pages/Contact/Contact"
+import Honors from "./pages/Honors/Honors"
 
 const routes = [
   { path: '/', element: <Home /> },
   { path: '/gallery', element: <Gallery /> },
   { path: '/blogs', element: <Blogs /> },
   { path: '/single-blog', element: <SingleBlog /> },
-  { path: '/contact', element: <Contact /> }
+  { path: '/contact', element: <Contact /> },
+  { path: '/honors', element: <Honors /> }
 ]
 
 export default routes
